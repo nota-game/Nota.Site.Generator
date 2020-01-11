695f21a5bf5267814ceeca186ad87f6de21c56b4
Patrick Kranz <patrick-kranz@live.de>
2020-01-11 15:45:09 +0100

    Git output added.

--- Nota.Site.Generator/Program.cs
+++ Nota.Site.Generator/Program.cs
@@ -1,12 +1,248 @@
-﻿using System;
+﻿using Stasistium;
+using Stasistium.Documents;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Nota.Site.Generator
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The Main Method
+        /// </summary>
+        /// <param name="configuration">Path to the configuration file (json)</param>
+        static async Task Main(FileInfo? configuration = null)
         {
-            Console.WriteLine("Hello World!");
+            configuration ??= new FileInfo("config.json");
+
+            if (!configuration.Exists)
+                throw new FileNotFoundException($"No configuration file was found ({configuration.FullName})");
+
+            using var context = new GeneratorContext();
+
+            var configFile = context.StageFromResult(configuration.FullName, x => x)
+             .File()
+             .Json()
+             .For<Config>();
+
+            var config = (await (await configFile.DoIt(null, new GenerationOptions().Token)).Perform).result.Value;
+
+            // Create the committer's signature and commit
+            var author = new LibGit2Sharp.Signature("NotaSiteGenerator", "@NotaSiteGenerator", DateTime.Now);
+            var committer = author;
+
+
+            LibGit2Sharp.Repository repo;
+            const string workdirPath = "gitOut";
+            const string cache = "cache";
+            const string output = "out";
+            if (Directory.Exists(workdirPath))
+            {
+                repo = new LibGit2Sharp.Repository(workdirPath);
+                LibGit2Sharp.Commands.Pull(repo, author, new LibGit2Sharp.PullOptions() { MergeOptions = new LibGit2Sharp.MergeOptions() { FastForwardStrategy = LibGit2Sharp.FastForwardStrategy.FastForwardOnly } });
+            }
+            else
+                repo = new LibGit2Sharp.Repository(LibGit2Sharp.Repository.Clone(config.WebsiteRepo ?? throw context.Exception($"{nameof(Config.SchemaRepo)} not set on configuration."), workdirPath));
+            using var repo2 = repo;
+
+
+            if (Directory.Exists(output))
+                Directory.Delete(output, true);
+
+            if (Directory.Exists(cache))
+                Directory.Delete(cache, true);
+
+            Directory.CreateDirectory(output);
+            foreach (var path in new DirectoryInfo(workdirPath).GetDirectories().Where(x => x.Name != ".git"))
+            {
+                if (path.Name == "cache")
+                    path.MoveTo(cache);
+                else
+                    path.MoveTo(Path.Combine(output, path.Name));
+            }
+
+
+
+            var contentRepo = configFile.Transform(x => x.With(x.Value.ContentRepo ?? throw x.Context.Exception($"{nameof(Config.ContentRepo)} not set on configuration."), x.Value.ContentRepo))
+                .GitModul();
+
+            var schemaRepo = configFile.Transform(x => x.With(x.Value.SchemaRepo ?? throw x.Context.Exception($"{nameof(Config.SchemaRepo)} not set on configuration."), x.Value.SchemaRepo).With(x.Metadata.Add(new HostMetadata() { Host = x.Value.Host })))
+                .GitModul();
+
+            var layoutProvider = configFile.Transform(x => x.With(x.Value.Layouts ?? "layout", x.Value.Layouts ?? "layout")).FileSystem().FileProvider("Layout");
+
+            var generatorOptions = new GenerationOptions()
+            {
+                CompressCache = false,
+                Refresh = false
+            };
+            var s = System.Diagnostics.Stopwatch.StartNew();
+            var files = contentRepo
+                .SelectMany(input =>
+                    input
+                    .Transform(x => x.With(x.Metadata.Add(new GitMetadata(x.Value.FrindlyName, x.Value.Type))))
+                    .GitRefToFiles()
+                    .Sidecar()
+                        .For<BookMetadata>(".metadata")
+                    .Where(x => System.IO.Path.GetExtension(x.Id) == ".md")
+                    .Select(x => x.Markdown().MarkdownToHtml().TextToStream())
+                    .Transform(x => x.WithId(Path.Combine(Enum.GetName(typeof(GitRefType), x.Metadata.GetValue<GitMetadata>()!.Type)!, x.Metadata.GetValue<GitMetadata>()!.Name, x.Id)))
+                );
+
+
+            var contentVersions = contentRepo.Transform(input =>
+            {
+                var gitData = input.Value;
+                string version;
+                if (gitData.Type == GitRefType.Branch && gitData.FrindlyName == "master")
+                    version = "vNext";
+                else if (gitData.Type == GitRefType.Branch)
+                    version = "draft/" + gitData.FrindlyName;
+                else
+                    version = gitData.FrindlyName;
+                return input.With(version, version).WithId(version);
+            })
+            .OrderBy(x => new VersionComparer(x.Id));
+
+            var razorProvider = files
+                .FileProvider("Content")
+                .Concat(layoutProvider)
+                .RazorProvider("Content", "Layout/ViewStart.cshtml");
+
+            var rendered = files.Select(x => x.Razor(razorProvider).TextToStream());
+            var hostReplacementRegex = new System.Text.RegularExpressions.Regex(@"(?<host>http://nota\.org)/schema/", System.Text.RegularExpressions.RegexOptions.Compiled);
+
+            var schemaFiles = schemaRepo
+
+                .SelectMany(input =>
+                 input
+                 .Transform(x => x.With(x.Metadata.Add(new GitMetadata(x.Value.FrindlyName, x.Value.Type))))
+                 .GitRefToFiles()
+                 .Where(x => System.IO.Path.GetExtension(x.Id) != ".md")
+                 .Select(x =>
+                    x.ToText()
+                    .Transform(y =>
+                    {
+                        var gitData = y.Metadata.GetValue<GitMetadata>()!;
+                        string version;
+
+                        if (gitData.Type == GitRefType.Branch && gitData.Name == "master")
+                            version = "vNext";
+                        else if (gitData.Type == GitRefType.Branch)
+                            version = "draft/" + gitData.Name;
+                        else
+                            version = gitData.Name;
+
+
+                        var host = y.Metadata.GetValue<HostMetadata>()!.Host;
+
+                        var newText = hostReplacementRegex.Replace(y.Value, @$"{host}/schema/{version}/");
+
+                        return y.With(newText, y.Context.GetHashForString(newText));
+                    })
+                    .TextToStream()
+                 )
+                 .Transform(x =>
+                 {
+                     var gitData = x.Metadata.GetValue<GitMetadata>()!;
+                     string version;
+
+                     if (gitData.Type == GitRefType.Branch && gitData.Name == "master")
+                         version = "vNext";
+                     else if (gitData.Type == GitRefType.Branch)
+                         version = "draft/" + gitData.Name;
+                     else
+                         version = gitData.Name;
+
+
+                     return x.WithId($"schema/{version}/{x.Id.TrimStart('/')}");
+                 })
+
+                        );
+
+            var g = rendered
+                .Transform(x => x.WithId(Path.ChangeExtension(x.Id, ".html")))
+                .Concat(schemaFiles)
+                .Persist(new DirectoryInfo(output), generatorOptions)
+                ;
+
+            await g.UpdateFiles().ConfigureAwait(false);
+
+            foreach (var path in new DirectoryInfo(output).GetDirectories())
+            {
+                path.MoveTo(Path.Combine(workdirPath, path.Name));
+            }
+            new DirectoryInfo(cache).MoveTo(Path.Combine(workdirPath, cache));
+
+
+            var status = repo.RetrieveStatus(new LibGit2Sharp.StatusOptions() { });
+            if (status.Any())
+            {
+                var filePaths = status.Select(mods => mods.FilePath);
+                foreach (var filePath in filePaths)
+                    repo.Index.Add(filePath);
+                repo.Index.Write();
+
+
+                // Commit to the repository
+                var commit = repo.Commit("Updated Build", author, committer, new LibGit2Sharp.CommitOptions() { });
+
+                repo.Network.Push(repo.Branches["master"], new LibGit2Sharp.PushOptions() { });
+            }
+
+            s.Stop();
+
+            context.Logger.Info($"Operation Took {s.Elapsed}");
+
+
+
+
         }
+    }
+
+    internal class Config
+    {
+        public string? ContentRepo { get; set; }
+        public string? SchemaRepo { get; set; }
+        public string? Layouts { get; set; }
+        public string? Host { get; set; }
+
+        public string? WebsiteRepo { get; set; }
+    }
+
+    internal class GitMetadata
+    {
+        public GitMetadata(string name, GitRefType type)
+        {
+            this.Name = name ?? throw new ArgumentNullException(nameof(name));
+            this.Type = type;
+        }
+
+        public string Name { get; }
+        public GitRefType Type { get; }
+    }
+
+    internal class BookMetadata
+    {
+        public string? Title { get; set; }
+        public int Chapter { get; set; }
+    }
+
+    internal class HostMetadata
+    {
+        public string? Host { get; set; }
+    }
+
+    /// <summary>
+    /// Contains the layout that should be used
+    /// </summary>
+    public class PageLayoutMetadata
+    {
+        /// <summary>
+        /// The Layout that should be used.
+        /// </summary>
+        public string? Layout { get; set; }
     }
 }
---
+++ Nota.Site.Generator/VersionComparer.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Nota.Site.Generator
+{
+    internal class VersionComparer : IComparable<VersionComparer>
+    {
+        private readonly NumberSting id;
+        private readonly bool thisIsDraft;
+        private readonly bool isVNext;
+
+        public VersionComparer(string id)
+        {
+            this.thisIsDraft = id.StartsWith("draft/");
+            this.isVNext = id == "vNext";
+
+
+            if (this.thisIsDraft)
+                this.id = new NumberSting(id.AsMemory("draft/".Length));
+            else
+                this.id = new NumberSting(id.AsMemory());
+
+        }
+
+        public int CompareTo([AllowNull] VersionComparer? other)
+        {
+
+            if (other is null)
+                return -1;
+
+            if (this.thisIsDraft && !other.thisIsDraft)
+                return 1;
+            if (this.thisIsDraft && other.thisIsDraft)
+                return this.id.CompareTo(other.id);
+            if (!this.thisIsDraft && other.thisIsDraft)
+                return -1;
+
+            if (this.isVNext && other.isVNext)
+                return 0;
+            if (this.isVNext)
+                return -1;
+
+            return this.id.CompareTo(other.id);
+        }
+
+
+        private class NumberSting : IComparable<NumberSting>
+        {
+            private readonly ReadOnlyCollection<object> data;
+
+            public NumberSting(ReadOnlyMemory<char> str)
+            {
+                int start = 0;
+                bool isDigit = char.IsDigit(str.Span[0]);
+                var list = new List<object>();
+                for (int i = 1; i < str.Length; i++)
+                {
+                    bool currentIsDigit = char.IsDigit(str.Span[i]);
+                    if (currentIsDigit == isDigit)
+                        continue;
+
+                    if (isDigit)
+                    {
+                        var number = int.Parse(str.Slice(start, i - start).Span);
+                        list.Add(number);
+                    }
+                    else
+                    {
+                        var memory = str.Slice(start, i - start);
+                        list.Add(memory);
+                    }
+                    isDigit = currentIsDigit;
+                    start = i;
+                }
+                this.data = list.AsReadOnly();
+            }
+
+            public int CompareTo([AllowNull] NumberSting other)
+            {
+
+                for (int i = 0; i < Math.Min(this.data.Count, other.data.Count); i++)
+                {
+                    var thisElement = this.data[i];
+                    var otherElement = other.data[i];
+
+                    if (thisElement is ReadOnlyMemory<char> && otherElement is int)
+                        return 1;
+                    else if (thisElement is int && otherElement is ReadOnlyMemory<char>)
+                        return -1;
+
+                    else if (thisElement is int thisI && otherElement is int otherI)
+                    {
+                        var c = thisI.CompareTo(otherI);
+                        if (c != 0)
+                            return c;
+                    }
+
+                    else if (thisElement is ReadOnlyMemory<char> thisStr && otherElement is ReadOnlyMemory<char> otherStr)
+                    {
+                        var c = thisStr.Span.CompareTo(otherStr.Span, StringComparison.InvariantCulture);
+                        if (c != 0)
+                            return c;
+                    }
+                    else
+                        throw new InvalidOperationException("This should not happen");
+                }
+
+                return this.data.Count.CompareTo(other.data.Count);
+
+            }
+
+        }
+
+
+
+    }
+}
